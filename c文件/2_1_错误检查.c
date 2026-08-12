5ab4cc8486995c1280c770d4ddf13f0896e3fe5f
Ron_Chen <RonChen666@outlook.com>
2026-08-12 09:40:42 +0800

    新增“C文件”文件夹，用于保存和手册同步的学习代码

---
+++ c文件/2_1_错误检查.c
@@ -0,0 +1,14 @@
+#include<errno.h>
+#include<stdio.h>
+
+
+int main() {
+//	errno.h
+	printf("errno为%d\n", errno);
+	
+	FILE *f = fopen("./nofile", "r");
+
+	printf("errno为%d\n", errno);
+	
+	return 0;
+}
